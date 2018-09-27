6da3dd37a79b76341a679018d22664f0c80449fb
Vladar4 <vladar4@gmail.com>
2018-09-27 18:39:20 +0300

    fix for Nim v0.19.0

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 cd src
 nim c --out:..\tbk.exe -d:release --opt:speed --app:gui ng2tbk.nim
-rmdir /s /q nimcache
 cd ..
 
--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd src
 nim c --out:../tbk -d:release --opt:speed ng2tbk.nim
-rm -rf nimcache
 cd ..
 
